ca2e8c238e935c0bd6c7fba5f4e060f4499431af
Aleš Roubíček <ales@roubicek.name>
2017-12-09 20:05:23 +0100

    Updated make script

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 # builds the mdsol/logspout-cloudwatch docker image
 set -xe
 
-docker build --no-cache --tag=mdsol/logspout -f docker/Dockerfile docker
+docker build --no-cache --tag=topmonks/logspout-cloudwatch -f docker/Dockerfile docker
